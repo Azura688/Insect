f3704ec480f43d1b1b88b47c5364f3e702f621e7
ZhangMinghui <8145931+HUIwuxiu@user.noreply.gitee.com>
2022-01-22 18:00:42 +0800

    解决计算时间段问题

--- ruoyi-insectdata/src/main/java/com/ruoyi/insectdata/service/impl/IdentificationServiceImpl.java
+++ ruoyi-insectdata/src/main/java/com/ruoyi/insectdata/service/impl/IdentificationServiceImpl.java
@@ -196,15 +196,12 @@ public class IdentificationServiceImpl implements IIdentificationService
 
     public static int daysBetween(Date smdate,Date bdate) throws ParseException//计算两个日期之间的天数
     {
-        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
-        smdate=sdf.parse(sdf.format(smdate));
-        bdate=sdf.parse(sdf.format(bdate));
         Calendar cal = Calendar.getInstance();
         cal.setTime(smdate);
         long time1 = cal.getTimeInMillis();
         cal.setTime(bdate);
         long time2 = cal.getTimeInMillis();
-        long between_days=(time2-time1)/(1000*3600*24);
+        long between_days=(time2-time1+1)/(1000*3600*24);
 
         return Integer.parseInt(String.valueOf(between_days));
     }
