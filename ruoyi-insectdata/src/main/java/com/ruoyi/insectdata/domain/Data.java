abac8bcbbb2199c6defe07627ac8b5976a963706
ZhangMinghui <8145931+HUIwuxiu@user.noreply.gitee.com>
2022-01-23 22:35:47 +0800

    photoTime查询到秒

--- ruoyi-insectdata/src/main/java/com/ruoyi/insectdata/domain/Data.java
+++ ruoyi-insectdata/src/main/java/com/ruoyi/insectdata/domain/Data.java
@@ -33,8 +33,8 @@ public class Data extends BaseEntity
     private String tagPicture;
 
     /** 拍摄时间 */
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    @Excel(name = "拍摄时间", width = 30, dateFormat = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Excel(name = "拍摄时间", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss")//
     private Date photoTime;
 
     /** 拍摄地点 */
